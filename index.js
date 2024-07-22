66881c77b185bbab45f5eee984e4a945828b7625
kenedy4 <kennedymusyoki8@gmail.com>
2024-07-22 17:39:17 +0300

    fixing hosting errors another error erupts

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Importing json-server library into this project
+const jsonServer = require("json-server");
+const server = jsonServer.create(); // Create a new server
+const router = jsonServer.router("db.json"); // Create a router using the JSON file
+const middlewares = jsonServer.defaults(); // Add default middlewares
+const port = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
+
 // Adding event listeners to the document to fire after loading html content
 document.addEventListener("DOMContentLoaded", () => {
   // Fetch suspects data and display them to the screen for user to take action
